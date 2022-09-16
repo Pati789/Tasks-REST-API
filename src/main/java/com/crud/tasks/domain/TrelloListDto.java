4fc820a109687980e75435f1f5452af210530ce0
Patrycja Brodkorb <https://github.com/Pati789/Patrycja-Brodkorb-kodilla-java.git>
2022-09-16 16:37:22 +0200

    1

--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrelloListDto {
 
     @JsonProperty("id")
